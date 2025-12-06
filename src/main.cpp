07c8debc5e86e1e97f3e4bba306c2c5faf02802e
whosting39-beep <whosting39@gmail.com>
2025-12-06 10:50:32 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -90,7 +90,7 @@ class $modify(MyLevelCell, LevelCell) {
             return;
         }
 
-        std::string URL = fmt::format("https://raw.githubusercontent.com/cdc-sys/level-thumbnails/main/thumbs/{}.png",(int)m_level->m_levelID);
+        std::string URL = fmt::format("https://raw.githubusercontent.com/whosting39-beep/level-thumbs-mod/main/thumbs/{}.png",(int)m_level->m_levelID);
         int id = m_level->m_levelID.value();
 
         auto req = web::WebRequest();
